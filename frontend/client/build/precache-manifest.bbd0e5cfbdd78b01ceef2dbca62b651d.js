41676d42fe3b823f9103e464dd7decf67f48eb8e
Modurotolu Olokode <durolawk@gmail.com>
2020-05-05 21:27:47 +0100

    deleted nested gitignore and ran client build

---
+++ frontend/client/build/precache-manifest.bbd0e5cfbdd78b01ceef2dbca62b651d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c62e43a3f4a287c5bb748992b14f7a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3689e26004a26ad89e0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "071a15e5846342df8a11",
+    "url": "/static/js/2.930c9f53.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.930c9f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3689e26004a26ad89e0",
+    "url": "/static/js/main.9dc678c6.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
