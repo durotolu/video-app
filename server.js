2f061003c1e0bd58f2dd77eb90a6819333a27f4e
Modurotolu Olokode <durolawk@gmail.com>
2020-05-05 21:06:29 +0100

    added dotenv environment

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.static(__dirname + '/frontend/client/public'))
 let clients = 0
