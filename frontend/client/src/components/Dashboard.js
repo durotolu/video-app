f9d22bb6d51bd7eb387c1504835feaad8e422561
Modurotolu Olokode <durolawk@gmail.com>
2020-08-02 10:49:15 +0100

    handled roomId state on click

--- frontend/client/src/components/Dashboard.js
+++ frontend/client/src/components/Dashboard.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
 import { Link, Route } from "react-router-dom";
 import { AiFillCaretRight } from 'react-icons/ai'
-import { v4 as uuidV4} from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
 
 import Call from './Call'
 
-function Dashboard({ handleChange }) {
+function Dashboard({ handleChange, handleRoomUrl, roomId, setRoomId }) {
   const [createInput, setCreateInput] = useState(true)
   const [joinInput, setJoinInput] = useState(true)
+
+  const handleClick = (input) => {
+    handleRoomUrl(roomId)
+    setRoomId(uuidV4())
+    if (input) {
+      setCreateInput(false)
+    } else {
+      setCreateInput(true)
+    }
+    // input ? setCreateInput(false) : setCreateInput(true)
+  }
 
   return (
     <div>
       <h2>Welcome to some Video Peer-to-Peer App</h2>
       <div>
-        <button onClick={() => createInput ? setCreateInput(false) : setCreateInput(true)}>create room</button>
+        <button onClick={() => handleClick(createInput)} >create room</button>
         <input onChange={handleChange} placeholder='enter id for new room' hidden={createInput} />
-        <Link to={`/call/${uuidV4()}`} hidden={createInput}><AiFillCaretRight /></Link>
+        <Link to={`/call/${roomId}`} hidden={createInput}><AiFillCaretRight /></Link>
       </div>
       <div>
         <button onClick={() => joinInput ? setJoinInput(false) : setJoinInput(true)}>join room</button>
