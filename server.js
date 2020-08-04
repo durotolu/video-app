5b5a52619433e864617713debb7779609b090f57
Modurotolu Olokode <durolawk@gmail.com>
2020-08-04 08:05:56 +0100

    added rooms feature

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
 require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const port = process.env.PORT
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT;
 
-app.use(express.static(__dirname + '/frontend/client/build'))
+// app.use(express.static(__dirname + '/frontend/client/build'))
 let clients = 0
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/frontend/client/public/index.html')
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/frontend/client/public/index.html')
+// });
 
 io.on('connection', function(socket) {
-  socket.on("NewClient", function() {
-    if(clients < 2) {
-      if(clients === 1) {
-        this.emit('CreatePeer')
-      }
-    }
-    else 
-      this.emit('SessionActive')
-    clients++
+  socket.on("joinRoom", function(roomId, userId) {
+    console.log(roomId, userId)
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('userConnected', userId)
+    // if(clients < 2) {
+    //   if(clients === 1) {
+    //     this.emit('CreatePeer')
+    //   }
+    // }
+    // else 
+    //   this.emit('SessionActive')
+    // clients++
   })
   socket.on('Offer', sendOffer)
   socket.on('Answer', sendAnswer)
